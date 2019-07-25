708e5856825c6291351dea2d7de18680ce072305
bestieee2020 <shkarface@gmail.com>
2019-07-25 21:47:08 +0300

    Added our first viewset and router

--- src/profiles_project/profiles_api/urls.py
+++ src/profiles_project/profiles_api/urls.py
@@ -1,6 +1,15 @@
 from django.conf.urls import url
+from django.conf.urls import include
+from rest_framework.routers import DefaultRouter
+
 from . import views
 
+
+router = DefaultRouter()
+router.register('hello.viewset', views.HelloViewSet, base_name='hello_viewset')
+
+
 urlpatterns = [
-url(r'^hello-view/', views.HelloApiView.as_view())
+url(r'^hello-view/', views.HelloApiView.as_view()),
+url(r'', include(router.urls))
 ]
--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+
+from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from . import serializers
+
 
 
 # Create your views here.
@@ -8,6 +14,8 @@ from rest_framework.response import Response
 class HelloApiView(APIView):
     """docstring for HelloApiView
 ."""
+
+    serializer_class = serializers.HelloSerializer
 
     def get(self,request,format=None):
 
@@ -18,3 +26,40 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message':'Hello', 'an_apiview' : an_apiview})
+
+    def post(self,request):
+
+        serializer = serializers.HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {0}'.format(name)
+            return Response({'message': message})
+
+        else:
+            return Response(
+                serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+
+    def put(self,request,pk=None):
+        return Response({'method':'put'})
+
+
+    def patch(self,request,pk=None):
+        return Response({'method':'patch'})
+
+
+    def delete(self,request,pk=None):
+        return Response({'method':'delete'})
+
+
+class HelloViewSet(viewsets.ViewSet):
+
+    def list(self,request):
+        a_viewset = [
+    'viewset list item no 1',
+    'viewset list item no 2',
+    'viewset list item no 3',
+    ]
+
+        return Response({'message':'Hello!', 'a_viewset': a_viewset})
